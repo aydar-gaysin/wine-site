ce03691accc09be710f797759b34de65fb147912
aydar.gaysin@gmail.com <aydar.gaysin@gmail.com>
2021-05-20 12:51:16 +0300

    Imported Jinja2 lib and code to render page Renamed index.html

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+render_page = template.render(
+
+)
+
+with open('template.html', 'w', encoding='utf8') as file:
+    file.write(render_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
