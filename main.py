7acd706b4fe874820450b5b2051896b6d87514ba
aydar.gaysin@gmail.com <aydar.gaysin@gmail.com>
2021-05-28 13:28:28 +0300

    Refactor reading from excel file with defaultdict(list) Rewrite template structure

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import collections
 import datetime
+
 import pandas
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from pprint import pprint
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -13,21 +15,18 @@ env = Environment(
 template = env.get_template('template.html')
 
 years_running = int(datetime.datetime.now().year) - 1920
-excel_data = pandas.read_excel('wine.xlsx')
-wine_bottles = []
-for index in range(6):
-    wine_bottles.append(
-        {
-            'title': excel_data.to_dict()['Название'][index],
-            'price': f'{excel_data.to_dict()["Цена"][index]} руб.',
-            'sort': excel_data.to_dict()['Сорт'][index],
-            'image': f'images/{excel_data.to_dict()["Картинка"][index]}'
-        }
-    )
+
+excel_data = pandas.read_excel('wine2.xlsx', na_values=['Nan', 'nan'],
+                               keep_default_na=False)
+
+sorted_wines = collections.defaultdict(list)
+for product in excel_data.to_dict(orient='records'):
+    category = product['Категория']
+    sorted_wines[category].append(product)
 
 render_page = template.render(
     years_running=years_running,
-    wine_bottles=wine_bottles,
+    sorted_wines=sorted_wines,
 )
 
 with open('index.html', 'w', encoding='utf8') as file:
