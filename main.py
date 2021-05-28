866ea04045045df11e30f61770f8bbd9d9a143d5
aydar.gaysin@gmail.com <aydar.gaysin@gmail.com>
2021-05-28 17:05:59 +0300

    Add requirements Add actions sticker for product images

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-
+YEAR_STARTED = 1920
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -14,9 +14,9 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-years_running = int(datetime.datetime.now().year) - 1920
+years_running = int(datetime.datetime.now().year) - YEAR_STARTED
 
-excel_data = pandas.read_excel('wine2.xlsx', na_values=['Nan', 'nan'],
+excel_data = pandas.read_excel('wine3.xlsx', na_values=['Nan', 'nan'],
                                keep_default_na=False)
 
 sorted_wines = collections.defaultdict(list)
