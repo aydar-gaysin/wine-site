5e055aab95ae2424546d2fd200e851e2a94d1b84
aydar.gaysin@gmail.com <4r8hMBYjPh5Gmyk>
2021-05-31 18:06:11 +0300

    Edit variable names

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-YEAR_STARTED = 1920
+YEAR_OF_FOUNDATION = 1920
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -14,18 +14,17 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-years_running = int(datetime.datetime.now().year) - YEAR_STARTED
-
-excel_data = pandas.read_excel('products.xlsx', na_values=['Nan', 'nan'],
-                               keep_default_na=False)
+years_functioning = int(datetime.datetime.now().year) - YEAR_OF_FOUNDATION
+products_import = pandas.read_excel('products.xlsx', na_values=['Nan', 'nan'],
+                                    keep_default_na=False)
 
 sorted_wines = collections.defaultdict(list)
-for product in excel_data.to_dict(orient='records'):
+for product in products_import.to_dict(orient='records'):
     category = product['Категория']
     sorted_wines[category].append(product)
 
 render_page = template.render(
-    years_running=years_running,
+    years_functioning=years_functioning,
     sorted_wines=sorted_wines,
 )
 
