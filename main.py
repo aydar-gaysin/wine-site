5f2a17ebc4eb669c5679fe560228dbae70f8ec6d
aydar.gaysin@gmail.com <4r8hMBYjPh5Gmyk>
2021-06-02 22:34:58 +0300

    Add dotenv library requirement Change method to get xlsx-file pathway from environment file

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import collections
 import datetime
+import os
 
 import pandas
 
+from dotenv import load_dotenv
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+load_dotenv()
+IMPORT_CATALOGUE = os.getenv('CATALOGUE_PATH')
 YEAR_OF_FOUNDATION = 1920
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -15,7 +20,7 @@ env = Environment(
 template = env.get_template('template.html')
 
 years_functioning = int(datetime.datetime.now().year) - YEAR_OF_FOUNDATION
-products_import = pandas.read_excel('products.xlsx', na_values=['Nan', 'nan'],
+products_import = pandas.read_excel(IMPORT_CATALOGUE, na_values=['Nan', 'nan'],
                                     keep_default_na=False)
 
 sorted_wines = collections.defaultdict(list)
